b9e4f38a81b594463ef05e628cdc9511f8779df4
Arnaud Blois <a.blois@ucl.ac.uk>
2021-03-06 19:03:47 +0000

    add apt-get dependencies + add sqlite support

--- build_python.sh
+++ build_python.sh
@@ -2,15 +2,30 @@
 
 set -e
 
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get install -y \
+  build-essential \
+  libffi-dev \
+  libgdbm-dev \
+  libncurses5-dev \
+  libnss3-dev \
+  libsqlite3-dev \
+  libssl-dev \
+  wget \
+  zlib1g-dev
+
 # Script building the required version of python
 read -p "Enter the version to build [3.9.2]: " version
 version=${version:-3.9.2}
-echo "Building Python version $version"
+echo "Building Python version ${version}"
 
 wget https://www.python.org/ftp/python/${version}/Python-${version}.tgz -O - | tar -xz
 cd Python-${version}
-./configure --enable-optimizations --with-lto
+./configure --enable-optimizations --with-lto --enable-loadable-sqlite-extensions
 make
 sudo make altinstall
 cd ..
 sudo rm -rf Python-${version}
+
+echo "Python ${version} has been successfully installed and is accessible at /usr/local/bin/python3."
